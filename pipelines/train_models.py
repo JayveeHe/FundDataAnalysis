61e1d39dd178001fab626f2c41e47c2b3f4d9276
Jayvee-Air <jayveehe@gmail.com>
2017-04-08 13:48:57 +0800

    注释掉冗余代码

--- pipelines/train_models.py
+++ pipelines/train_models.py
@@ -11,18 +11,14 @@ from __future__ import division
 import cPickle
 import multiprocessing
 import os
-import random
 
 import sys
 
-from lightgbm import Booster
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print 'Related File:%s\t----------project_path=%s' % (__file__, PROJECT_PATH)
 sys.path.append(PROJECT_PATH)
 
-from pipelines.analysis_data import test_datas_wrapper
-from pipelines.data_preprocess import load_csv_data
 from utils.logger_utils import data_process_logger
 from utils.model_utils import train_with_lightgbm
 import cPickle
@@ -114,60 +110,60 @@ def train_lightGBM_new_data(train_file_number_list, former_model=None, output_li
                         thread_num=process_count)
 
 
-def test_old_datas():
-    model_tag = 'dart_iter30000_norm_combined_400000full'
-    train_datas = []
-    # load with multi-processor
-    process_count = 12
-    proc_pool = multiprocessing.Pool(process_count)
-    multi_results = []
-    for i in range(200, 401):
-        # data_process_logger.info('loading %s file' % i)
-        csv_path = '%s/datas/%s.csv' % (PROJECT_PATH, i)
-        data_res = proc_pool.apply_async(load_csv_data, args=(csv_path, True, True))
-        multi_results.append(data_res)
-        # datas = load_csv_data(csv_path, normalize=True, is_combine=True)
-        # train_datas += datas
-    proc_pool.close()
-    proc_pool.join()
-    # fetch datas from pool
-    data_process_logger.info('combining csv datas...')
-    for i in xrange(len(multi_results)):
-        datas = multi_results[i].get()
-        train_datas += datas
-        # dump normalized train datas
-    data_process_logger.info('dumping norm datas...')
-    cPickle.dump(train_datas, open('%s/datas/norm_datas/200_norm_combined_datas_full.dat' % PROJECT_PATH, 'wb'),
-                 protocol=2)
-    # load train normalized train datas
-    # data_process_logger.info('loading datas...')
-    # train_datas = cPickle.load(open('%s/datas/norm_datas/200_norm_datas_full.dat' % PROJECT_PATH, 'rb'))
-    # random sample the train datas
-    # SAMPLE_SIZE = 20000
-    # data_process_logger.info('random sampling %s obs...' % SAMPLE_SIZE)
-    # random.shuffle(train_datas)
-    # train_datas = train_datas[:SAMPLE_SIZE]
-    # ------- start training ---------
-    # output_gbrt_path = '%s/models/gbrt_%s.model' % (PROJECT_PATH, model_tag)
-    # train_gbrt_model(train_datas, output_gbrt_path)
-
-    # output_svr_path = '%s/models/svr_%s.model' % (PROJECT_PATH, model_tag)
-    # train_svr_model(train_datas, output_svr_path)
-
-    output_lightgbm_path = '%s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag)
-    # lightgbm_params = {'learning_rates': lambda iter_num: 0.05 * (0.99 ** iter_num)}
-    train_with_lightgbm(train_datas, output_lightgbm_path, num_boost_round=30000, early_stopping_rounds=150,
-                        learning_rates=lambda iter_num: max(0.8 * (0.99 ** iter_num), 0.0005))
-    # --------- Testing -------
-    data_process_logger.info('--------LightGBM:----------')
-    data_process_logger.info('using model: %s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag))
-    lightgbm_mod = cPickle.load(open('%s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag), 'rb'))
-    # data_process_logger.info('test trianing file')
-    # test_datas_wrapper(range(1,100),lightgbm_mod)
-    data_process_logger.info('test test file')
-    # print  list(lightgbm_mod.feature_importances_)
-    test_datas_wrapper([100, 150, 200, 310], lightgbm_mod, is_combined=True, normalize=True)
-    test_datas_wrapper(range(500, 551), lightgbm_mod, is_combined=True, normalize=True)
+# def test_old_datas():
+#     model_tag = 'dart_iter30000_norm_combined_400000full'
+#     train_datas = []
+#     # load with multi-processor
+#     process_count = 12
+#     proc_pool = multiprocessing.Pool(process_count)
+#     multi_results = []
+#     for i in range(200, 401):
+#         # data_process_logger.info('loading %s file' % i)
+#         csv_path = '%s/datas/%s.csv' % (PROJECT_PATH, i)
+#         data_res = proc_pool.apply_async(load_csv_data, args=(csv_path, True, True))
+#         multi_results.append(data_res)
+#         # datas = load_csv_data(csv_path, normalize=True, is_combine=True)
+#         # train_datas += datas
+#     proc_pool.close()
+#     proc_pool.join()
+#     # fetch datas from pool
+#     data_process_logger.info('combining csv datas...')
+#     for i in xrange(len(multi_results)):
+#         datas = multi_results[i].get()
+#         train_datas += datas
+#         # dump normalized train datas
+#     data_process_logger.info('dumping norm datas...')
+#     cPickle.dump(train_datas, open('%s/datas/norm_datas/200_norm_combined_datas_full.dat' % PROJECT_PATH, 'wb'),
+#                  protocol=2)
+#     # load train normalized train datas
+#     # data_process_logger.info('loading datas...')
+#     # train_datas = cPickle.load(open('%s/datas/norm_datas/200_norm_datas_full.dat' % PROJECT_PATH, 'rb'))
+#     # random sample the train datas
+#     # SAMPLE_SIZE = 20000
+#     # data_process_logger.info('random sampling %s obs...' % SAMPLE_SIZE)
+#     # random.shuffle(train_datas)
+#     # train_datas = train_datas[:SAMPLE_SIZE]
+#     # ------- start training ---------
+#     # output_gbrt_path = '%s/models/gbrt_%s.model' % (PROJECT_PATH, model_tag)
+#     # train_gbrt_model(train_datas, output_gbrt_path)
+#
+#     # output_svr_path = '%s/models/svr_%s.model' % (PROJECT_PATH, model_tag)
+#     # train_svr_model(train_datas, output_svr_path)
+#
+#     output_lightgbm_path = '%s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag)
+#     # lightgbm_params = {'learning_rates': lambda iter_num: 0.05 * (0.99 ** iter_num)}
+#     train_with_lightgbm(train_datas, output_lightgbm_path, num_boost_round=30000, early_stopping_rounds=150,
+#                         learning_rates=lambda iter_num: max(0.8 * (0.99 ** iter_num), 0.0005))
+#     # --------- Testing -------
+#     data_process_logger.info('--------LightGBM:----------')
+#     data_process_logger.info('using model: %s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag))
+#     lightgbm_mod = cPickle.load(open('%s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag), 'rb'))
+#     # data_process_logger.info('test trianing file')
+#     # test_datas_wrapper(range(1,100),lightgbm_mod)
+#     data_process_logger.info('test test file')
+#     # print  list(lightgbm_mod.feature_importances_)
+#     test_datas_wrapper([100, 150, 200, 310], lightgbm_mod, is_combined=True, normalize=True)
+#     test_datas_wrapper(range(500, 551), lightgbm_mod, is_combined=True, normalize=True)
 
 
 if __name__ == '__main__':
