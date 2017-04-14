5e32b49cd3fa9c378378cd8b756f7ac07a447e4e
Jayvee@Jinhu <jayveehe@gmail.com>
2017-04-15 00:52:03 +0800

    修改analysis的输出文件名tag

--- pipelines/analysis_data.py
+++ pipelines/analysis_data.py
@@ -211,8 +211,8 @@ def result_validation(ranked_index_ylist, N=50, threshold=0.35):
 if __name__ == '__main__':
     # --------- Testing -------
     # model_tag = 'New_Quant_Data_rebalanced_norm_gbdt_7leaves_iter30000_best'
-    # model_tag = 'Full_gbdt_15leaves'
-    model_tag = 'Full_gbdt_7leaves_iter50000'
+    model_tag = 'Full_gbdt_15leaves'
+    # model_tag = 'Full_gbdt_7leaves_iter50000'
     data_process_logger.info('--------LightGBM:----------')
     data_process_logger.info('using model: %s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag))
     # lightgbm_mod = cPickle.load(open('%s/models/lightgbm_%s.model' % (PROJECT_PATH, model_tag), 'rb'))
@@ -225,7 +225,7 @@ if __name__ == '__main__':
     f_numbers, f_rank_rates = test_quant_data_wrapper(
         range(1,300)+range(401,840)+range(941,1042)+range(1145,1200)+range(1301,1400)+range(1511,1521),
         lightgbm_mod,
-        normalize=True, predict_iteration=50000)
+        normalize=True, predict_iteration=60000)
     # f_numbers, f_rank_rates = test_quant_data_wrapper(
     #     range(1, 11), lightgbm_mod,
     #     normalize=True)
@@ -233,7 +233,8 @@ if __name__ == '__main__':
     #     range(1, 11), lightgbm_mod,
     #     normalize=True, process_count=2)
     # save test result to csv
-    with open('%s/pipelines/test_%s_result_%s.csv' % (PROJECT_PATH, model_tag, len(f_numbers)), 'wb') as fout:
+    result_tag = 'iter6w'
+    with open('%s/pipelines/test_%s_%s_result_%s.csv' % (PROJECT_PATH, model_tag, result_tag, len(f_numbers)), 'wb') as fout:
         for i in range(len(f_numbers)):
             fout.write('%s,%s\n' % (f_numbers[i], f_rank_rates[i]))
         data_process_logger.info('result csv: %s/pipelines/test_%s_result_%s.csv' % (PROJECT_PATH,model_tag, len(f_numbers)))
