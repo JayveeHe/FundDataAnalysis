9cc0bf82d501cf6b23a6eda7adc2f3e332c30bc4
Jiawei <hejiawei03@meituan.com>
2017-08-27 04:32:12 +0800

    updata data preprocess

--- pipelines/data_preprocess.py
+++ pipelines/data_preprocess.py
@@ -5,6 +5,7 @@ Created by jayvee on 17/3/3.
 https://github.com/JayveeHe
 """
 import csv
+import gzip
 import os
 import sys
 
@@ -135,8 +136,8 @@ def infer_missing_datas(fin_csv_path, fout_pickle_path, is_norm=False, is_norm_s
         for line in reader:
             if len(line) == n_feature:
                 single_vec_value = [float(i) if i != 'NaN' else np.nan for i in line]
-                 # process the 453th col, remove future feature.
-                single_vec_value = single_vec_value[:453]+single_vec_value[454:]
+                # process the 453th col, remove future feature.
+                single_vec_value = single_vec_value[:453] + single_vec_value[454:]
                 origin_datas.append(single_vec_value)
                 # data_process_logger.info('handled line %s' % count)
 
@@ -167,6 +168,78 @@ def infer_missing_datas(fin_csv_path, fout_pickle_path, is_norm=False, is_norm_s
         return transformed_datas
 
 
+def infer_missing_datas_to_gzip(fin_csv_path, fout_gzip_path, is_norm=True, is_norm_score=True):
+    """
+    处理NaN数据,并将处理后的数据分别存储为csv与pickle文件
+    Args:
+        is_norm: 是否进行标准化
+        is_norm_score: 是否对score进行标准化
+        fin_csv_path:
+        fout_gzip_path:
+
+    Returns:
+
+    """
+    with open(fin_csv_path, 'rb') as fin_csv, \
+            gzip.open(fout_gzip_path, 'wb') as fout_gzip:
+        origin_datas = []
+        reader = csv.reader(fin_csv)
+        # writer = csv.writer(fout_csv)
+        count = 1
+        n_feature = 4563
+        data_process_logger.info('start reading %s' % fin_csv_path)
+        for line in reader:
+            if len(line) == n_feature:
+                single_vec_value = [float(i) if i != 'NaN' else np.nan for i in line]
+                # process the 453th col, remove future feature.
+                single_vec_value = single_vec_value[:453] + single_vec_value[454:]
+                origin_datas.append(single_vec_value)
+                # data_process_logger.info('handled line %s' % count)
+
+            else:
+                data_process_logger.info(
+                    'casting line: %s in file %s, it has %s features while the first line has %s' % (
+                        count, fin_csv_path, len(line), n_feature))
+            count += 1
+        # inferring missing data
+        imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+        imp.fit(origin_datas)
+        transformed_datas = imp.transform(origin_datas)
+        stock_ids = []
+        stock_scores = []
+        scaled_vec_values = []
+        if is_norm:
+            # standardising datas
+            stock_ids = transformed_datas[:, 0]
+            stock_scores = transformed_datas[:, 1]
+            vec_values = transformed_datas[:, 2:]
+            scaled_vec_values = preprocessing.scale(vec_values)
+            if is_norm_score:
+                stock_scores = preprocessing.scale(stock_scores)
+            transformed_datas = (stock_ids.tolist(), stock_scores.tolist(), scaled_vec_values.tolist())  # 存为tuple
+        # writting transformed datas
+        # data_process_logger.info('start writting %s' % fout_csv_path)
+        data_process_logger.info('start saving %s' % fout_gzip_path)
+        # transformed_datas = transformed_datas.tolist()  # 转为list进行存储
+        # cPickle.dump(transformed_datas, fout_gzip, protocol=2)
+        for line_index in xrange(len(transformed_datas[0])):
+            stock_id = stock_ids[line_index]
+            stock_score = stock_scores[line_index]
+            scaled_vec_value = scaled_vec_values[line_index]
+            tmp_vec = [int(stock_id)] + [float(stock_score)] + scaled_vec_value.tolist()
+            tmp_vec_str = [str(a) for a in tmp_vec]
+            try:
+                # tmp_list_vec = tmp_vec.tolist()
+                tmp_line = ','.join(tmp_vec_str)
+                fout_gzip.write(tmp_line + '\n')
+                if line_index % 100 == 0:
+                    print 'line %s join success' % line_index
+            except Exception, e:
+                print 'line %s join failed, details=%s' % (line_index, e)
+        data_process_logger.info('%s done' % fin_csv_path)
+        return transformed_datas
+
+
 def parallel_inferring(file_number_list, process_count=12, is_norm=True, is_norm_score=True, data_root_path=None):
     """
     并行化进行数据清理
@@ -184,12 +257,14 @@ def parallel_inferring(file_number_list, process_count=12, is_norm=True, is_norm
         fin_csv_path = '%s/%s.csv' % (data_root_path, i)
         if is_norm:
             # fout_csv_path = '%s/transformed_datas/%s_trans_norm.csv' % (data_root_path, i)
-            fout_pickle_path = '%s/pickle_datas/%s_trans_norm.pickle' % (data_root_path, i)
+            # fout_pickle_path = '%s/pickle_datas/%s_trans_norm.pickle' % (data_root_path, i)
+            fout_gzip_path = '%s/gzip_datas/%s_trans_norm.gz' % (data_root_path, i)
         else:
             # fout_csv_path = '%s/transformed_datas/%s_trans.csv' % (data_root_path, i)
-            fout_pickle_path = '%s/pickle_datas/%s_trans.pickle' % (data_root_path, i)
+            # fout_pickle_path = '%s/pickle_datas/%s_trans.pickle' % (data_root_path, i)
+            fout_gzip_path = '%s/gzip_datas/%s_trans.gz' % (data_root_path, i)
         data_res = proc_pool.apply_async(infer_missing_datas,
-                                         args=(fin_csv_path, fout_pickle_path, is_norm, is_norm_score))
+                                         args=(fin_csv_path, fout_gzip_path, is_norm, is_norm_score))
         # multi_results.append(data_res)
         # datas = load_csv_data(csv_path, normalize=True, is_combine=True)
         # train_datas += datas
@@ -200,11 +275,12 @@ def parallel_inferring(file_number_list, process_count=12, is_norm=True, is_norm
 
 if __name__ == '__main__':
     # print len(load_csv_data('%s/datas/%s.csv' % (PROJECT_PATH, 1), is_combine=True))
-    # infer_missing_datas(fin_csv_path='%s/datas/Quant-Datas/%s.csv' % (PROJECT_PATH, 1),
-    #                     fout_csv_path='%s/datas/Quant-Datas/transformed_datas/%s_trans.csv' % (PROJECT_PATH, 1),
-    #                     fout_pickle_path='%s/datas/Quant-Datas/pickle_datas/%s_trans.pickle' % (PROJECT_PATH, 1))
+    # infer_missing_datas_to_gzip(
+    #     fin_csv_path='/Users/jayveehe/git_project/FundDataAnalysis/pipelines/datas/tmp_data/9.csv',
+    #     fout_gzip_path='/Users/jayveehe/git_project/FundDataAnalysis/pipelines/datas/tmp_data/9_non_normvec_non_normscore.gz',
+    #     is_norm=True, is_norm_score=False)
     # pickle_data = cPickle.load()
     # print len(pickle_data)
     d_r_p = '%s/datas/Quant_Datas_v3.0' % (DATA_ROOT)
-    parallel_inferring(file_number_list=range(1, 1511), process_count=32, is_norm=True, is_norm_score=False,
+    parallel_inferring(file_number_list=range(1, 1511), process_count=28, is_norm=True, is_norm_score=False,
                        data_root_path=d_r_p)
